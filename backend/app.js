ce103e402ee01f221cdd5c26200b3b0f7732af81
Your Name <you@example.com>
2025-06-16 15:04:44 -0600

    Documentacion swagger

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import tasksRoutes from "./src/routes/tasksRoutes.js";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
 
 // Creo una constante que es igual a la libreria que importé
 const app = express();
@@ -17,7 +20,13 @@ app.use(cors({
 app.use(express.json());
 //Que acepte cookies en postman
 app.use(cookieParser());
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve("./auto_tasks.json"), "utf-8")
+  );
+
 // Definir las rutas de las funciones que tendrá la página web
 app.use("/api/tasks", tasksRoutes);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Exporto la constante para poder usar express en otros archivos
 export default app;
